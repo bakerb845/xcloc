f9ecc522060d5c5fefdc52a9e5815d1b4f751170
Ben <bakerb845@gmail.com>
2017-11-14 09:32:45 -0800

    improving hte envelope computation by adding parallelism

--- envelope.c
+++ envelope.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <string.h>
 #include "xcloc_envelope.h"
+#include <mkl.h>
 #include <ipps.h>
 
 #ifndef M_PI
@@ -14,8 +15,29 @@ static void sinc(const int n,
                  const double *__restrict__ x,
                  double *__restrict__ sinc);
 
+/*!
+ * @brief Performs the FIR Hilbert transform design.  It is a complex valued
+ *        Kaiser window filter design.  For more information see 12.4 of
+ *        Oppenheim and Schafer - Discrete Time Signals Processing 3rd ed.
+ *
+ * @param[in] n          Number of samples in filter.
+ * @param[in] precision  Controls whether the filter will be for single or
+ *                       double precision signals.
+ * @param[in] accuracy   Controls the accuracy when computing the magnitude
+ *                       of the Hilbert transform.  Lower accuracy results
+ *                       in better performance.
+ *
+ * @param[out] envelope  Envelope structure with the FIR Hilbert filter design
+ *                       and work-space sizes.
+ *
+ * @result 0 indicates success.
+ *
+ * @copyright Ben Baker distributed under the MIT license.
+ *
+ */
 int xcloc_envelope_initialize(const int n,
                               const enum xclocPrecision_enum precision,
+                              const enum xclocAccuracy_enum accuracy,
                               struct xclocEnvelope_struct *envelope)
 {
     const Ipp64f beta = 8.0;
@@ -35,6 +57,13 @@ int xcloc_envelope_initialize(const int n,
         precision != XCLOC_DOUBLE_PRECISION)
     {
         fprintf(stderr, "%s: Invalid precision\n", __func__);
+        return -1;
+    }
+    if (accuracy != XCLOC_HIGH_ACCURACY &&
+        accuracy != XCLOC_MEDIUM_ACCURACY &&
+        accuracy != XCLOC_EXTENDED_ACCURACY)
+    {
+        fprintf(stderr, "%s: Invalid accuracy\n", __func__);
         return -1;
     }
     // Create the kaiser window
@@ -72,7 +101,7 @@ int xcloc_envelope_initialize(const int n,
     }
     ippsDivC_64f_I(gain, hfiltR, n);
     ippsDivC_64f_I(gain, hfiltI, n);
-    // Fix the Type III case
+    // Fix the Type IV case
     if (n%2 == 1)
     {
         for (i=0; i<n; i++)
@@ -83,6 +112,7 @@ int xcloc_envelope_initialize(const int n,
         }
     }
     envelope->tapsLen = n;
+    if (envelope->tapsLen%2 == 1){envelope->ltype4 = true;}
     // Copy filter coefficients
     if (precision == XCLOC_SINGLE_PRECISION)
     {
@@ -121,66 +151,150 @@ printf("%e %e\n", hfiltR[i], hfiltI[i]);
     ippsFree(hfiltI);
     return 0;
 }
-
+//============================================================================//
+/*!
+ * @brief Finalizes the FIR filter structure.
+ *
+ * @param[out] envelope   On exit all memory has been released and all 
+ *                        variables set to 0.
+ *
+ * @result 0 indicates success.
+ *
+ * @copyright Ben Baker distributed under the MIT license.
+ *
+ */
 int xcloc_envelope_finalize(struct xclocEnvelope_struct *envelope)
 {
+    if (!envelope->linit){return 0;}
+    if (envelope->hfiltR != NULL){ippsFree(envelope->hfiltR);}
+    if (envelope->hfiltI != NULL){ippsFree(envelope->hfiltI);}
     memset(envelope, 0, sizeof(struct xclocEnvelope_struct));
     return 0;
 }
+//============================================================================//
+/*!
+ * @brief Computes the envelope of a group of signals.
+ *
+ * @param[in] nsignals       Number of signals.
+ * @param[in] lds            Leading dimension of x and xfilt.
+ * @param[in] npts           Number of points in signals.
+ * @param[in] precision      Precision of the input signals.
+ * @param[in,out] envelope   On input contains the initialized envelope
+ *                           structure. \n
+ *                           On exit some workspace variables may be updated.
+ * @param[in] x              Contains the signals of which to compute the
+ *                           envelope.  This is an array of dimension 
+ *                           [lds x nsignals] whose precision, float or
+ *                           double, is dictated by the precision variable.
+ * @param[out] xfilt         The envelope of the input signals.  This is an
+ *                           array of dimension [lds x nsignals] whose,
+ *                           single or double, is dicated by the precision
+ *                           variable. 
+ *                           
+ * @result 0 indicates success.
+ *
+ * @copyright Ben Baker distributed under the MIT license.
+ *
+ */
 int xcloc_envelope_apply(const int nsignals,
                           const int lds, const int npts, 
                           const enum xclocPrecision_enum precision,
-                          struct xclocEnvelope_struct envelope,
+                          struct xclocEnvelope_struct *envelope,
                           const void *__restrict__ x,
                           void *__restrict__ xfilt)
 {
     IppsFIRSpec_32f *pSpec32R, *pSpec32I;
     Ipp8u *pBuf;
-    Ipp32f *xmean32, *xwork32R, *xwork32I, pMean32;
-    int filterLen, is, winLen2;
+    Ipp32f *hfiltR32, *hfiltI32, *ptr32R, *ptr32I, *xmean32,
+           *xwork32R, *xwork32I, pMean32;
+    int bufferSize, filterLen, is, specSize, tapsLen, winLen2;
+    bool ltype4;
     size_t indx;
-    if (!envelope.linit)
+    enum xclocAccuracy_enum accuracy;
+    if (!envelope->linit)
     {
         fprintf(stderr, "%s: envelope not initialized\n", __func__);
         return -1;
     }
-    if (precision != envelope.precision)
+    if (precision != envelope->precision)
     {
         fprintf(stderr, "%s: Mixed precision not yet handled\n", __func__);
         return -1;
     }
-    winLen2 = envelope.tapsLen/2;
+    ltype4 = envelope->ltype4;
+    winLen2 = envelope->tapsLen/2;
     filterLen = npts + winLen2;
-    if (envelope.precision == XCLOC_SINGLE_PRECISION)
+    tapsLen = envelope->tapsLen;
+    accuracy = envelope->accuracy;
+    specSize = envelope->specSize;
+    bufferSize = envelope->bufferSize;
+    if (envelope->precision == XCLOC_SINGLE_PRECISION)
     {
-        pSpec32R = (IppsFIRSpec_32f *) ippsMalloc_8u(envelope.specSize);
-        pSpec32I = (IppsFIRSpec_32f *) ippsMalloc_8u(envelope.specSize);
-        pBuf = ippsMalloc_8u(envelope.bufferSize);
-        ippsFIRSRInit_32f(envelope.hfiltR, envelope.tapsLen,
-                          ippAlgAuto, pSpec32R);
-        ippsFIRSRInit_32f(envelope.hfiltI, envelope.tapsLen,
-                          ippAlgAuto, pSpec32I);
+        hfiltR32 = (Ipp32f *) envelope->hfiltR;
+        hfiltI32 = (Ipp32f *) envelope->hfiltI;
+        #pragma omp parallel default(none) \
+         shared(accuracy, bufferSize, hfiltR32, hfiltI32, filterLen, ltype4, \
+                npts, specSize, tapsLen, x, xfilt, winLen2) \
+         private(indx, is, pBuf, pMean32, pSpec32R, pSpec32I, ptr32R, ptr32I, \
+                 xmean32, xwork32R, xwork32I)
+        {
+        pSpec32R = (IppsFIRSpec_32f *) ippsMalloc_8u(specSize);
+        pSpec32I = (IppsFIRSpec_32f *) ippsMalloc_8u(specSize);
+        pBuf = ippsMalloc_8u(bufferSize);
+        ippsFIRSRInit_32f(hfiltR32, tapsLen, ippAlgAuto, pSpec32R);
+        ippsFIRSRInit_32f(hfiltI32, tapsLen, ippAlgAuto, pSpec32I);
         xwork32I = ippsMalloc_32f(filterLen);
         xwork32R = ippsMalloc_32f(filterLen);
         xmean32  = ippsMalloc_32f(filterLen);
-        ippsZero_32f(xmean32, filterLen);
+        ippsZero_32f(xwork32R, filterLen);
+        ippsZero_32f(xwork32I, filterLen);
+        ippsZero_32f(xmean32,  filterLen);
         // Loop on the transforms signals and compute envelope
+        #pragma omp for
         for (is=0; is<nsignals; is++)
         {
             // Demean the signal
+            indx = (size_t) (is*lds)*sizeof(float);
             ippsMean_32f(&x[indx], npts, &pMean32, ippAlgHintFast);
             ippsSubC_32f(&x[indx], pMean32, xmean32, npts);
-            indx = (size_t) (is*lds)*sizeof(float);
-            ippsFIRSR_32f(xmean32, xwork32R, filterLen, pSpec32R,
-                          NULL, NULL, pBuf);
-            ippsFIRSR_32f(xmean32, xwork32I, filterLen, pSpec32I,
-                          NULL, NULL, pBuf); 
+            if (ltype4)
+            {
+                // The real part of the FIR filter is just a delay of 
+                // winLen/2 samples  
+                //ippsCopy_32f(xmean32, &xwork32R[winLen2], npts);
+                // TODO - This is actually a sparse filter
+                ippsFIRSR_32f(xmean32, xwork32I, filterLen, pSpec32I,
+                              NULL, NULL, pBuf);
+                // Handle the phase delay
+                ptr32R = (Ipp32f *) &xmean32[0];
+                ptr32I = (Ipp32f *) &xwork32I[winLen2];
+            }
+            else
+            {
+                // Apply the complex filter
+                ippsFIRSR_32f(xmean32, xwork32R, filterLen, pSpec32R,
+                              NULL, NULL, pBuf);
+                ippsFIRSR_32f(xmean32, xwork32I, filterLen, pSpec32I,
+                              NULL, NULL, pBuf);
+                ptr32R = (Ipp32f *) &xwork32R[winLen2];
+                ptr32I = (Ipp32f *) &xwork32I[winLen2];
+            }
             // Compute the absolute value of the Hilbert transform.  Note,
             // winLen2 this removes the phase delay.
-            ippsMagnitude_32f(&xwork32R[winLen2], &xwork32I[winLen2],
-                              xmean32, npts);
-            // Re-instate the mean into the signal
-            ippsAddC_32f(xmean32, pMean32, &xfilt[indx], npts); 
+            if (accuracy == XCLOC_MEDIUM_ACCURACY)
+            {
+                vmsHypot(npts, ptr32R, ptr32I, xmean32, VML_LA);
+            }
+            else if (accuracy == XCLOC_HIGH_ACCURACY)
+            {
+                vmsHypot(npts, ptr32R, ptr32I, xmean32, VML_HA);
+            }
+            else
+            {
+                vmsHypot(npts, ptr32R, ptr32I, xmean32, VML_EP);
+            }
+            // Reincorporate the mean into the signal
+            ippsAddC_32f(xmean32, pMean32, &xfilt[indx], npts);
         }
         ippsFree(pSpec32R);
         ippsFree(pSpec32I);
@@ -188,6 +302,7 @@ int xcloc_envelope_apply(const int nsignals,
         ippsFree(xwork32I);
         ippsFree(xwork32R);
         ippsFree(xmean32);
+        }
     }
     else
     {
--- include/xcloc_envelope.h
+++ include/xcloc_envelope.h
@@ -6,13 +6,26 @@
 
 struct xclocEnvelope_struct
 {
-    void *hfiltR;
-    void *hfiltI;
-    int tapsLen;
-    int specSize;
-    int bufferSize;
-    enum xclocPrecision_enum precision;
-    bool linit;
+    void *hfiltR;             /*!< Real coefficients of Hilbert FIR 
+                                   transform.  This is an Ipps32f or
+                                   Ipps64f array of dimension [tapsLne]. */
+    void *hfiltI;             /*!< Imaginary coefficients of Hilbert FIR
+                                   transform.  This is an Ipps32f or 
+                                   Ipps64f array of dimension [tapsLen]. */
+    int tapsLen;              /*!< Number of taps in FIR filters. */
+    int specSize;             /*!< Size of workspace for FIR state. */
+    int bufferSize;           /*!< Size of workspace for filter. */
+    enum xclocAccuracy_enum
+         accuracy;            /*!< Controls numerical precision in envelope
+                                   computation. */
+    enum xclocPrecision_enum
+         precision;           /*!< Precision of filter.  This can be
+                                    single or double precision. */
+          
+    bool ltype4;              /*!< If true then the FIR transform is of
+                                   Type 4 (tapsLen is odd).  This results in
+                                   a lot of sparseness. */
+    bool linit;               /*!< If true then the filter was initialized. */
 };
 
 #ifdef __cplusplus
@@ -23,15 +36,17 @@ extern "C"
 /*! Initialize the envelope structure. */
 int xcloc_envelope_initialize(const int n,
                               const enum xclocPrecision_enum precision,
+                              const enum xclocAccuracy_enum accuracy,
                               struct xclocEnvelope_struct *envelope);
 /*! Apply the envelope. */
 int xcloc_envelope_apply(const int nsignals,
                           const int lds, const int npts, 
                           const enum xclocPrecision_enum precision,
-                          struct xclocEnvelope_struct envelope,
+                          struct xclocEnvelope_struct *envelope,
                           const void *__restrict__ x,
                           void *__restrict__ xfilt);
-
+/*! Finalizes the envelope structure. */
+int xcloc_envelope_finalize(struct xclocEnvelope_struct *envelope);
 #ifdef __cplusplus
 }
 #endif 
