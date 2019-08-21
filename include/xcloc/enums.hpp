646fb620ea84004bfbc825108156b1c445d05c90
Ben Baker <bakerb845@gmail.com>
2019-08-20 18:09:04 -0600

    forgot a file

---
+++ include/xcloc/enums.hpp
@@ -0,0 +1,31 @@
+#ifndef XCLOC_ENUMS_HPP
+#define XCLOC_ENUMS_HPP
+namespace XCLoc
+{
+
+/*!
+ * @brief Defines the floating point precision.
+ */
+enum class Precision
+{
+    DOUBLE, /*!< Uses double (64 bit) precision calculations. */
+    FLOAT   /*!< Uses float (32 bit) precision calcuations. */
+};
+/*!
+ * @brief Defines the floating point accuracy used in some of the floating
+ *        point computations in MKL.
+ */
+enum class MKLFloatingPointAccuracy
+{
+    HIGH_ACCURACY, /*!< The default high floating point accuracy. */ 
+    LOW_ACCURACY,  /*!< This offers better perfformance than HIGH_ACCURACY.
+                        For double precision this has about 12 digits of
+                        accuracy.  For single precision this has about 4 digits
+                        of accuracy. */
+    EP_ACCURACY    /*!< This offers the best performance but for double 
+                        precision this may have less than 8 digits of accuracy
+                        and for float precision this may have less than four
+                        digits of accuracy.  This mode is not recommended. */
+};
+}
+#endif
